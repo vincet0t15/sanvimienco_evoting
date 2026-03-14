a24fe3850d366504707b189e0a907cc883aeb3a3
vincet0t15 <vincet0t15@gmail.com>
2026-03-14 20:09:31 +0800

    asdf

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,5 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
-class EventController extends Controller {}
+class EventController extends Controller
+{
+    public function index(Request $request)
+    {
+        return Inertia::render('Events/index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
@@ -9,6 +10,9 @@ Route::inertia('/', 'welcome', [
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+
+    // Events
+    Route::get('events', [EventController::class, 'index'])->name('events.index');
 });
 
-require __DIR__.'/settings.php';
+require __DIR__ . '/settings.php';
