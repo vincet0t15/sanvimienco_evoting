3191a71bf71b664c8c6e203e81e27a04c69c2e64
vincet0t15 <vincet0t15@gmail.com>
2026-03-14 20:02:39 +0800

    asfd

---
+++ app/Models/Event.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Event extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'start_at',
+        'end_at',
+    ];
+}
