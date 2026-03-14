738b46a42a4bf8089112640a5d278b7083b48295
vincet0t15 <vincet0t15@gmail.com>
2026-03-14 20:01:02 +0800

    asdf

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -27,7 +27,6 @@ class CreateNewUser implements CreatesNewUsers
         return User::create([
             'name' => $input['name'],
             'username' => $input['username'],
-            'email' => $input['username'].'@local.test',
             'password' => $input['password'],
         ]);
     }
